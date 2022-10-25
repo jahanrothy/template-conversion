4b0a05d8843187b3e3a483c3cf9a8a9fed740039
Rothy <jahanrothy58@gamil.com>
2022-10-26 00:52:54 +0600

    changing className

--- src/component/samplePost/SamplePage.component.js
+++ src/component/samplePost/SamplePage.component.js
@@ -27,8 +27,8 @@ export default function SamplePage(){
         {/* <!-- Post Content--> */}
         <article className="mb-4">
             <div className="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
+                <div className="row gx-4 gx-lg-5 justify-content-center">
+                    <div className="col-md-10 col-lg-8 col-xl-7">
                         <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>
 
                         <p>Science cuts two ways, of course; its products can be used for both good and evil. But there's no turning back from science. The early warnings about technological dangers also come from science.</p>
@@ -55,7 +55,7 @@ export default function SamplePage(){
                         <a href="#!"><img class="img-fluid" src="assets/img/post-sample-image.jpg" alt="..." /></a>
                         <span class="caption text-muted">To go places and do things that have never been done before – that’s what living is all about.</span>
                         <p>Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no man has gone before.</p>
-                        
+
                         <p>As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a fundamental truth to our nature, Man must explore, and this is exploration at its greatest.</p>
                         <p>
                             Placeholder text by
